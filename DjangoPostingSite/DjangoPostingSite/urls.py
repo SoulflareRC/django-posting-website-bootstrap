1a8e752f08b76e874949257223334717d86f211b
SoulflareRC <chenrc91@gmail.com>
2023-09-03 13:50:30 -0400

    fixed Dockerfile & compose.yml

--- DjangoPostingSite/DjangoPostingSite/urls.py
+++ DjangoPostingSite/DjangoPostingSite/urls.py
@@ -40,5 +40,5 @@ urlpatterns = [
     path("",lambda request:redirect('posts:index')) # redirect to post index page
 
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
+# if settings.DEBUG:
+urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
